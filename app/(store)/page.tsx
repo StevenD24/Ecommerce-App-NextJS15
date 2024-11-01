d6e506b15ca7d01be2164baadfde5a95bc19be5a
Steven Duong <stevenduongx@gmail.com>
2024-11-01 15:15:26 -0600

    install shadcn

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>hello world</h1>
+      <Button>click me</Button>
     </div>
   );
 }
